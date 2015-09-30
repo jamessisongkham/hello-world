8ff30c6dc0e2599296f909137c8c50fcbaa1843d
james sisongkham <jamessisongkham@ninja15.d22.lan>
2015-09-30 07:33:22 -0500

    dgbrfbgf

---
+++ NetBeansProjects/sisongkham_4_prime_methods/src/sisongkham_4_prime_methods/Sisongkham_4_prime_methods.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sisongkham_4_prime_methods;
+
+/**
+ *
+ * @author jamessisongkham
+ */
+public class Sisongkham_4_prime_methods {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        for(int i = 0;i < 100;i++){
+            if(isOdd(i)){
+                System.out.println(i + "is odd");
+                
+            }
+                }
+        }
+    static boolean isOdd(int n){
+        return (n % 2 == 0); 
+    }
+ static boolean isEven(int n){
+        return (n % 2 == 0); 
+}
+   static boolean isPrime(int somenumber){
+       int devisor = 0;
+       for (int i = 2; i < somenumber; i++) {
+           if (somenumber % i == 0) {
+               devisor += 1;
+           }
+       
+   if
+       
+   
+       
+       
+   
+       
+   
+   
+   
+   
+       
+   
+       
+      
+   
+       
+  
+       
+  
+   
+   
+   
+
+
+
+
+
+
+        // TODO code application logic here
+    
+/*
+method - is like a little program
+it can receive and return values to other methods
+*/
+
