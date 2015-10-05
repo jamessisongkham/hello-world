cdd8c5b47d647f8ddb667ff092249421b93477a8
james sisongkham <jamessisongkham@ninja15.d22.lan>
2015-10-05 07:16:29 -0500

    james sisongkham datatypes

---
+++ NetBeansProjects/sisongkham_4_datatypes/src/sisongkham_4_datatypes/Sisongkham_4_datatypes.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sisongkham_4_datatypes;
+
+/**
+ *
+ * //space for comments 
+ * //used to explain code
+ * code explanation is an expectation
+ * //in d22
+ * 
+ * @author jamessisongkham
+ */
+public class Sisongkham_4_datatypes {
+    int  i = 512;
+    float a = 7.11f;
+    double b = 11.0;
+    short c = 100;
+    char d1 = '1';
+    char d2 = 'd';
+    String e = "d22";
+    String el = "amanda is the best";
+    boolean f = true;
+    boolean g = i % 2 == 0;
+    
+            
+    
+    
+    
+    
+            
+    /**
+     * @param args the command line arguments
+     */
+    
+    public static void main(String[] args) {
+    }
+        // TODO code application logic here
+        /*% checks for the remainder
+        // i% 2 checks if a number
+        // even or odd */
+    
+    // anumber value for char
+    // maps to a character 
+    { i = 22 ;
+    a = 0 ; 
+    e = "ninja"; 
+}
+
+    /*adding an int  and a string 
+    con
+    */
+    for ( i=0; i < 500; i++) }
+/*if is a condition (flow) 
+statement. ifaq condition is true the following commandwill be 
+executed */
+if (i%2==0){
+system.out.println(i + "is even"}
+}else { 
+system.out.println(i+ " is odd")
++ "")
+}
+}
+    
+    
+ 
